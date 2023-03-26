4d302470621caac93ec7b0a5d472fc18f1f333b3
MohamedHayBOUALLAGUI <haithem.bouallagui90@gmail.com>
2023-03-26 14:04:32 +0100

    Spring MVC coté serveur-Formulaires et Validation

--- src/main/java/com/bouallagui/produits/controllers/ProduitController.java
+++ src/main/java/com/bouallagui/produits/controllers/ProduitController.java
@@ -5,10 +5,13 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 //import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -21,27 +24,39 @@ public class ProduitController {
 
 	@Autowired
 	private ProduitService produitService;
-	
-	
+
 	@RequestMapping("/showCreate")
-	public String showCreate() {
-		return "createProduit";
+	public String showCreate(ModelMap modelMap) {
+		modelMap.addAttribute("produit", new Produit());
+		modelMap.addAttribute("mode", "newProduct");
+		return "formProduit";
 	}
+
+//	@RequestMapping("/saveProduit")
+//	public String saveProduit(@ModelAttribute("produit") Produit produit, @RequestParam("date") String date,
+//			ModelMap modelMap) throws ParseException {
+//		// conversion de la date
+//		SimpleDateFormat dateformat = new SimpleDateFormat("yyyy-MM-dd");
+//		Date dateCreation = dateformat.parse(String.valueOf(date));
+//		produit.setDateCreation(dateCreation);
+//
+//		Produit saveProduit = produitService.saveProduit(produit);
+//		String msg = "produit enregistré avec Id " + saveProduit.getId();
+//		modelMap.addAttribute("msg", msg);
+//		return "createProduit";
+//	}
 	
 	@RequestMapping("/saveProduit")
-	public String saveProduit(@ModelAttribute("produit")  Produit produit, 
-	 @RequestParam("date") String date,ModelMap modelMap) throws ParseException {
-	//conversion de la date 
-	 SimpleDateFormat dateformat = new SimpleDateFormat("yyyy-MM-dd");
-	 Date dateCreation = dateformat.parse(String.valueOf(date));
-	 produit.setDateCreation(dateCreation);
-	 
-	 Produit saveProduit = produitService.saveProduit(produit);
-	String msg ="produit enregistré avec Id "+saveProduit.getId();
-	modelMap.addAttribute("msg", msg);
-	return "createProduit";
+	public String saveProduit(@Valid Produit produit, BindingResult bindingResul) 
+	{
+		if(bindingResul.hasErrors()) {
+			return "formProduit";
+		}
+		produitService.saveProduit(produit);
+		return "redirect:/ListeProduits";
 	}
-	
+
+
 //	@RequestMapping("/ListeProduits")
 //	public String listeProduits(ModelMap modelMap){
 //		
@@ -51,27 +66,24 @@ public class ProduitController {
 //		
 //		
 //	}
-	
-	//liste des produits avec pagination
+
+	// liste des produits avec pagination
 	@RequestMapping("/ListeProduits")
-	public String listeProduits(ModelMap modelMap,
-			@RequestParam (name="page",defaultValue = "0") int page,
-			@RequestParam (name="size", defaultValue = "2") int size){
-			Page<Produit> produits = produitService.getAllProduitsParPage(page, size);
-			modelMap.addAttribute("produits", produits);
-			//tableau d'entiers de taille nombre total de pages
-			modelMap.addAttribute("pages", new int[produits.getTotalPages()]);
-			modelMap.addAttribute("currentPage", page);
-			return "listeProduits";
+	public String listeProduits(ModelMap modelMap, @RequestParam(name = "page", defaultValue = "0") int page,
+			@RequestParam(name = "size", defaultValue = "2") int size) {
+		Page<Produit> produits = produitService.getAllProduitsParPage(page, size);
+		modelMap.addAttribute("produits", produits);
+		// tableau d'entiers de taille nombre total de pages
+		modelMap.addAttribute("pages", new int[produits.getTotalPages()]);
+		modelMap.addAttribute("currentPage", page);
+		return "listeProduits";
 	}
-	
-	
-	
+
 	@RequestMapping("/supprimerProduit")
-	public String supprimerProduit(ModelMap modelMap,@RequestParam("id") Long id,@RequestParam (name="page",defaultValue = "0") int page,
-			@RequestParam (name="size", defaultValue = "2") int size){
-		
-		
+	public String supprimerProduit(ModelMap modelMap, @RequestParam("id") Long id,
+			@RequestParam(name = "page", defaultValue = "0") int page,
+			@RequestParam(name = "size", defaultValue = "2") int size) {
+
 		produitService.deleteProduitById(id);
 //		List<Produit> produits = produitService.getAllProduits();
 //		modelMap.addAttribute("produits", produits);
@@ -82,38 +94,33 @@ public class ProduitController {
 //		modelMap.addAttribute("size", size);
 		return "redirect:/ListeProduits";
 	}
-	
+
 	@RequestMapping("/modifierProduit")
-	public String editerProduit(@RequestParam("id") Long id,ModelMap modelMap){
-	 Produit produit= produitService.getProduit(id);
-	modelMap.addAttribute("produit", produit);
-	return "editerProduit";
+	public String editerProduit(@RequestParam("id") Long id, ModelMap modelMap) {
+		Produit produit = produitService.getProduit(id);
+		modelMap.addAttribute("produit", produit);
+		modelMap.addAttribute("mode", "editProduct");
+		//return "editerProduit";
+		return "formProduit";
 	}
-	
+
 	@RequestMapping("/updateProduit")
-	public String updateProduit(@ModelAttribute("produit")  Produit produit,
-	@RequestParam("date") String date){
-		//conversion de la date 
-		 SimpleDateFormat dateformat = new SimpleDateFormat("yyyy-MM-dd");
-		 Date dateCreation;
+	public String updateProduit(@ModelAttribute("produit") Produit produit, @RequestParam("date") String date) {
+		// conversion de la date
+		SimpleDateFormat dateformat = new SimpleDateFormat("yyyy-MM-dd");
+		Date dateCreation;
 		try {
 			dateCreation = dateformat.parse(String.valueOf(date));
-			 produit.setDateCreation(dateCreation);
+			produit.setDateCreation(dateCreation);
 		} catch (ParseException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
-		 
-		 produitService.updateProduit(produit);
+
+		produitService.updateProduit(produit);
 //		 List<Produit> prods = produitService.getAllProduits();
 //		 modelMap.addAttribute("produits", prods);
 		return "redirect:/ListeProduits";
-		}
+	}
 
-
-
-	
-	
-	
 }
--- src/main/java/com/bouallagui/produits/entities/Produit.java
+++ src/main/java/com/bouallagui/produits/entities/Produit.java
@@ -7,6 +7,15 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.PastOrPresent;
+import javax.validation.constraints.Size;
+
+import org.springframework.format.annotation.DateTimeFormat;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -22,11 +31,17 @@ public class Produit {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	
+	@NotNull
+	@Size (min = 4,max = 20)
 	private String libelle;
 	
+	@Min(value = 10)
+	@Max(value = 10000)
 	private Double prix;
 	
-	
+	@Temporal(TemporalType.DATE)
+	@DateTimeFormat(pattern = "yyyy-MM-dd")
+	@PastOrPresent
 	private Date dateCreation;
 	
 	
